4f80b3561e6a500d512b87423c824f74476cd567
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-09 18:01:34 +0300

    create actions

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+import * as types from '../constants/ActionTypes';
+
+export const transformBlock = options => ({
+  type: types.TRANSFORM_BLOCK,
+  options
+});
+
+export const nextDetail = detail => ({
+  type: types.NEXT_DETAIL,
+  detail
+});
+
+export const downBlock = options => ({
+  type: types.DOWN_BLOCK,
+  options
+});
+
+export const moveDetail = side => ({
+  type: types.MOVE_DETAIL,
+  side
+});
+
+export const rotateDetail = () => ({type: types.ROTATE_DETAIL});
+
+export const completeRow = z => ({
+  type: types.COMPLETE_ROW,
+  z
+});
+
+export const setDisplay = filter => ({
+  type: types.SET_DISPLAY,
+  filter
+});
