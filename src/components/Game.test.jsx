9da0addbba44d0dfc9b0ad7fbcdcbcf32b641269
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-12 20:58:44 +0300

    create test to <Game />

---
+++ src/components/Game.test.jsx
@@ -0,0 +1,37 @@
+/* eslint
+  no-undef: 0,
+  no-unused-vars: 0
+*/
+import React from 'react';
+import {render, shallow, mount} from 'enzyme';
+import Game from './Game';
+import Row from './Row';
+import initialWorld from '../library/initialWorld';
+
+describe('<Game />', () => {
+  const x = 10;
+  const z = 24;
+  const world = initialWorld(z, x);
+  const wrapper = shallow(<Game world={world} />);
+
+  it('render without throwing an error', () => {
+    expect(wrapper.contains(
+      <Row
+        blocks={world.get(5).get('blocks')}
+        key={world.get(5).get('id')}
+      />
+    )).toBe(true);
+  });
+
+  it('render className', () => {
+    wrapper.find('.game', {}, result => expect(result.length).toBe(1));
+  });
+
+  it('render twenty <Row />', () => {
+    wrapper.find(Row, {}, result => expect(result.length).toBe(20));
+  });
+
+  it('render first <Row /> with id zero', () => {
+    wrapper.find(Row, {}, result => expect(result[0].get('id')).toBe(0));
+  });
+});
