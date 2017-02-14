9d53acad68113a8b526621b4aa8d45d1a54387b0
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-14 04:38:08 +0300

    add to add raw handle actions NEXT_DETAIL

--- src/reducers/world.js
+++ src/reducers/world.js
@@ -1,6 +1,37 @@
 import * as types from '../constants/ActionTypes';
 import initialWorld from '../library/initialWorld';
 
+const getStateWithNextDetail = (state, action) => {
+  const rangeFilter = (start, size) => (x, dx) =>
+    start > x || x > start + size || dx >= size;
+
+  console.log(action.detail);
+  const getRangeFilter = (detail) => {
+    if (detail.get(0).size < 2) return rangeFilter(4, 2); // 2 x 2
+    if (detail.get(0).size < 3) return rangeFilter(3, 3); // 3 x 3
+    return rangeFilter(3, 4); // 4 x 4
+  };
+
+  const rangeFilterDetail = getRangeFilter(action.detail);
+  const shadow = state.filter((row, y) => y < 0);
+
+  const shadowWithDetail = shadow.map((row, y) => {
+    let dx = 0; // detail x
+    if (y > action.detail.size) return row;
+
+    return row.get('blocks').map((block, x) => {
+      if (rangeFilterDetail(x, dx)) return block;
+      return {
+        id: block.get('id'),
+        value: action.detail.get(y).get(dx++)
+      };
+    });
+  });
+
+  return state.merge(shadowWithDetail);
+};
+
+
 const world = (state = initialWorld(24, 10), action) => {
   switch (action.type) {
   case types.DOWN_BLOCK:
@@ -10,7 +41,7 @@ const world = (state = initialWorld(24, 10), action) => {
   case types.MOVE_DETAIL:
     return state;
   case types.NEXT_DETAIL:
-    return state;
+    return getStateWithNextDetail(state, action);
   case types.ROTATE_DETAIL:
     return state;
   case types.COMPLETE_ROW:
