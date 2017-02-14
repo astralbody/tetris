0ce42cff52d5489051ef9064c15253e76017ff1a
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-14 21:49:11 +0300

    fix method create list, List.of -> List

--- src/constants/ShapeDetail.js
+++ src/constants/ShapeDetail.js
@@ -1,14 +1,14 @@
 import {List} from 'immutable';
 
-export const I = List.of([
+export const I = List([
   [0, 0, 0, 0],
   [2, 2, 2, 2],
   [0, 0, 0, 0],
   [0, 0, 0, 0]
 ]);
-export const T = List.of([[0, 0, 0], [2, 2, 2], [0, 2, 0]]);
-export const L = List.of([[0, 0, 0], [2, 2, 2], [2, 0, 0]]);
-export const J = List.of([[0, 0, 0], [2, 2, 2], [0, 0, 2]]);
-export const S = List.of([[0, 0, 0], [0, 2, 2], [2, 2, 0]]);
-export const Z = List.of([[0, 0, 0], [2, 2, 0], [0, 2, 2]]);
-export const O = List.of([[2, 2], [2, 2]]);
+export const T = List([[0, 0, 0], [2, 2, 2], [0, 2, 0]]);
+export const L = List([[0, 0, 0], [2, 2, 2], [2, 0, 0]]);
+export const J = List([[0, 0, 0], [2, 2, 2], [0, 0, 2]]);
+export const S = List([[0, 0, 0], [0, 2, 2], [2, 2, 0]]);
+export const Z = List([[0, 0, 0], [2, 2, 0], [0, 2, 2]]);
+export const O = List([[2, 2], [2, 2]]);
