7500ef3eba7897797dfb0f4db2d4ffb5076700cd
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-14 04:36:15 +0300

    fix constants ShapeDetail, add matrix

--- src/constants/ShapeDetail.js
+++ src/constants/ShapeDetail.js
@@ -1,9 +1,14 @@
 import {List} from 'immutable';
 
-export const I = List.of([0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0]);
-export const T = List.of([0, 0, 0, 2, 2, 2, 0, 2, 0]);
-export const L = List.of([0, 0, 0, 2, 2, 2, 2, 0, 0]);
-export const J = List.of([0, 0, 0, 2, 2, 2, 0, 0, 2]);
-export const S = List.of([0, 0, 0, 0, 2, 2, 2, 2, 0]);
-export const Z = List.of([0, 0, 0, 2, 2, 0, 0, 2, 2]);
-export const O = List.of([1, 1, 1, 1]);
+export const I = List.of([
+  [0, 0, 0, 0],
+  [2, 2, 2, 2],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+]);
+export const T = List.of([[0, 0, 0], [2, 2, 2], [0, 2, 0]]);
+export const L = List.of([[0, 0, 0], [2, 2, 2], [2, 0, 0]]);
+export const J = List.of([[0, 0, 0], [2, 2, 2], [0, 0, 2]]);
+export const S = List.of([[0, 0, 0], [0, 2, 2], [2, 2, 0]]);
+export const Z = List.of([[0, 0, 0], [2, 2, 0], [0, 2, 2]]);
+export const O = List.of([[2, 2], [2, 2]]);
