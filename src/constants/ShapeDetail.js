40529558ad44571e9be6793ea041d6806f536e12
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-16 15:05:10 +0300

    replace in ShapeDetail, List() on fromJS()

--- src/constants/ShapeDetail.js
+++ src/constants/ShapeDetail.js
@@ -1,14 +1,14 @@
-import {List} from 'immutable';
+import {fromJS} from 'immutable';
 
-export const I = List([
+export const I = fromJS([
   [0, 0, 0, 0],
   [2, 2, 2, 2],
   [0, 0, 0, 0],
   [0, 0, 0, 0]
 ]);
-export const T = List([[0, 0, 0], [2, 2, 2], [0, 2, 0]]);
-export const L = List([[0, 0, 0], [2, 2, 2], [2, 0, 0]]);
-export const J = List([[0, 0, 0], [2, 2, 2], [0, 0, 2]]);
-export const S = List([[0, 0, 0], [0, 2, 2], [2, 2, 0]]);
-export const Z = List([[0, 0, 0], [2, 2, 0], [0, 2, 2]]);
-export const O = List([[2, 2], [2, 2]]);
+export const T = fromJS([[0, 0, 0], [2, 2, 2], [0, 2, 0]]);
+export const L = fromJS([[0, 0, 0], [2, 2, 2], [2, 0, 0]]);
+export const J = fromJS([[0, 0, 0], [2, 2, 2], [0, 0, 2]]);
+export const S = fromJS([[0, 0, 0], [0, 2, 2], [2, 2, 0]]);
+export const Z = fromJS([[0, 0, 0], [2, 2, 0], [0, 2, 2]]);
+export const O = fromJS([[2, 2], [2, 2]]);
