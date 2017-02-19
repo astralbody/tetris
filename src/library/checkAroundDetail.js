4e5d92b71ce16238a353059e7b956c565dcf6b6d
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-19 12:09:52 +0300

    add comment to checkAroundDetail()

--- src/library/checkAroundDetail.js
+++ src/library/checkAroundDetail.js
@@ -1,7 +1,19 @@
+// Support
 export const inc = n => n + 1;
 export const dec = n => n - 1;
 export const echo = n => n;
 
+/**
+ * Check blocks around detail.
+ *
+ * @func
+ * @param {Immutable.Map} worldMap Map comprises blocks.
+ * @param {number} x Axis x.
+ * @param {number} y Axis y.
+ * @param {Function} fx Use to param x.
+ * @param {Function} fy Use to param y.
+ * @return {boolean} Free place lowwer blocks.
+ */
 export const checkAroundDetail = (worldMap, x, y, fx, fy) => {
   if (!worldMap.has(y)) return false;
 
