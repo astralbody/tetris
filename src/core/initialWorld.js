ae64022598df7e01997e8b6fef65349f929e0a40
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-02-27 07:51:50 +0300

    split initialWorld -> initialWorld() & initialRow()

--- src/core/initialWorld.js
+++ src/core/initialWorld.js
@@ -1,6 +1,14 @@
 import {fromJS} from 'immutable';
 import counter from './counter';
 
+const initialRow = (x, fRowID = Math.random, fBlockID = Math.random) => ({
+  blocks: Array.from({length: x}, block => ({
+    id: fBlockID(),
+    value: 0
+  })),
+  id: fRowID()
+});
+
 const initialWorld = (y, x) => {
   const counterRow = counter(-4);
   const counterBlock = counter(0);
@@ -8,29 +16,26 @@ const initialWorld = (y, x) => {
   return fromJS({
     map: Array.from(
       {length: y},
-      row => ({
-        blocks: Array.from({length: x}, block => ({
-          id: counterBlock(),
-          value: 0
-        })),
-        id: counterRow()
-      })
+      row => initialRow(x, counterRow, counterBlock)
     ),
     information: {
       currentDetail: {
         kind: 'T',
         pointX: 0,
-        pointZ: 0,
+        pointY: 0,
         size: 0
       },
       nextDetail: {
         kind: 'T',
         pointX: 0,
-        pointZ: 0,
+        pointY: 0,
         size: 0
       }
     }
   });
 };
 
-export default initialWorld;
+export {
+  initialWorld,
+  initialRow
+};
