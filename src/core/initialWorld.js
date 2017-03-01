0e04b5b8156d682e09f5c1bb9b32d3ba31f3734b
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-03-01 22:10:50 +0300

    fix conflict git

--- src/core/initialWorld.js
+++ src/core/initialWorld.js
@@ -1,11 +1,7 @@
 import {fromJS} from 'immutable';
 import counter from './counter';
 
-<<<<<<< HEAD
 export const initialRow = (x, fRowID = Math.random, fBlockID = Math.random) => ({
-=======
-const initialRow = (x, fRowID = Math.random, fBlockID = Math.random) => ({
->>>>>>> d4ee576b5ab0f80c482c26f253823ef729e3aeeb
   blocks: Array.from({length: x}, block => ({
     id: fBlockID(),
     value: 0
@@ -13,11 +9,7 @@ const initialRow = (x, fRowID = Math.random, fBlockID = Math.random) => ({
   id: fRowID()
 });
 
-<<<<<<< HEAD
 export const initialWorld = (y, x) => {
-=======
-const initialWorld = (y, x) => {
->>>>>>> d4ee576b5ab0f80c482c26f253823ef729e3aeeb
   const counterRow = counter(-4);
   const counterBlock = counter(0);
 
@@ -42,11 +34,3 @@ const initialWorld = (y, x) => {
     }
   });
 };
-<<<<<<< HEAD
-=======
-
-export {
-  initialWorld,
-  initialRow
-};
->>>>>>> d4ee576b5ab0f80c482c26f253823ef729e3aeeb
