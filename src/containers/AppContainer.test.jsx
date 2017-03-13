caee8903171366a579b83ab70a669b59ecb4b7b9
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-03-13 15:12:46 +0300

    create test to <AppContainer />

---
+++ src/containers/AppContainer.test.jsx
@@ -0,0 +1,19 @@
+/* eslint
+  no-undef: 0,
+  no-unused-vars: 0
+*/
+import React from 'react';
+import {shallow} from 'enzyme';
+import {Provider} from 'react-redux';
+import AppContainer from './AppContainer';
+import {initialWorld} from '../core/initialWorld';
+import App from '../components/App';
+import store from '../store/index';
+
+const world = initialWorld(10, 24);
+
+test('render <AppContainer />', () => expect(shallow(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>
+).contains(<AppContainer />)).toBe(true));
