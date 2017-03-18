ec7502255d1668f45422c04354ca2a2833096637
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-03-18 21:24:27 +0300

    refactoring getNextDetail()

--- src/core/getNextDetail.js
+++ src/core/getNextDetail.js
@@ -7,39 +7,38 @@ const getNextDetail = (state, action) => {
     action.detail.get('POINT_X'),
     action.detail.get('SIZE')
   );
+
   const shadow = state.get('map').filter((row, y) => row.get('id') < 0);
+
   const shadowWithDetail = shadow.map((row, y) => {
-    let nextSizeDetail = 0; // detail x
+    let nextSizeDetail = 0;
     if (y >= action.detail.get('SIZE')) return row;
 
-    return Map({
-      id: row.get('id'),
-      blocks: row.get('blocks').map((block, x) => {
-        if (rangeFilterDetail(x)) return block;
+    return row.set('blocks', row.get('blocks').map((block, x) => {
+      if (rangeFilterDetail(x)) return block;
 
-        return block.set(
-          'value',
-          action.detail
-            .get('BODY')
-            .get(y)
-            .get(nextSizeDetail++)
-        );
-      })
-    });
+      return block.set(
+        'value',
+        action.detail
+          .get('BODY')
+          .get(y)
+          .get(nextSizeDetail++)
+      );
+    }));
   });
 
-  return Map({
-    map: state.get('map').merge(shadowWithDetail),
-    info: Map({
-      nextDetail: Map({
-        kind: action.detail.get('KIND'),
-        pointX: action.detail.get('POINT_X'),
-        pointY: action.detail.get('POINT_Y'),
-        size: action.detail.get('SIZE')
-      }),
-      currentDetail: state.get('info').get('currentDetail')
-    })
-  });
+  return state
+    .set('map', state.get('map').merge(shadowWithDetail))
+    .setIn(['info', 'nextDetail'], Map({
+      kind: action.detail.get('KIND'),
+      pointX: action.detail.get('POINT_X'),
+      pointY: action.detail.get('POINT_Y'),
+      size: action.detail.get('SIZE')
+    }))
+    .setIn(
+      ['info', 'currentDetail'],
+      state.get('info').get('currentDetail')
+    );
 };
 
 export default getNextDetail;
