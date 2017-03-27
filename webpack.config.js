a50319ecd0d7cb0e0b29f403948ce526cc799498
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-03-27 20:04:41 +0300

    add config webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack/hot/only-dev-server',
+    'webpack-dev-server/client?http://localhost:9000',
+    './src/index.jsx'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }, {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, {
+        test: /\.(js|jsx)$/,
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'dist')
+        ],
+        enforce: 'pre',
+        use: 'eslint-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
