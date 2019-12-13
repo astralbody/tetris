751a0beb65566ec95f2e2cc1d1ad37026cf6c509
Artem Kryvchun <abstractmagicalwand@gmail.com>
2019-12-13 16:33:53 +0200

    Refactor structure of tests

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,8 +1,12 @@
 module.exports ={
+  roots: ['../src/', '.'],
+  setupFiles: [
+    '<rootDir>/setupFiles/enzyme.js',
+    '<rootDir>/setupFiles/localStorage.js',
+  ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
-  setupFiles: ['<rootDir>/tests/setupEnzyme.js', '<rootDir>/tests/setupLocalStorage'],
 };
